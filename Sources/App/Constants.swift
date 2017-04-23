aace90ca1f79eae4153a9258b32e43636e3de68c
Ashish Kapoor <swiftobjc@gmail.com>
2017-04-23 22:58:53 +0530

    added darksky api

---
+++ Sources/App/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  fun-with-vapor
+//
+//  Created by Dev on 23/04/17.
+//
+//
+
+import Foundation
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,9 +1,21 @@
 import Vapor
+import HTTP
+import Foundation
 
 let drop = Droplet()
 
 let weather = WeatherController()
-
 weather.addRoutes(drop:drop)
+
+drop.get { request in
+    let json = try drop.client.get("https://api.darksky.net/forecast/\(darkSkyKey)/37.8267,-122.4233", query:["exclude":"minutely,daily,alerts,flags"])
+    
+    let body        = try JSON(bytes: json.body.bytes!)
+    let currently   = body["currently"]! as JSON
+    let hourly      = body["hourly"]! as JSON
+    
+    return try JSON(node: ["currently": currently,
+                           "hourly": hourly])
+}
 
 drop.run()
