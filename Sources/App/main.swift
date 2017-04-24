1fb9a6fa9dcf084fef58ad7af2b1fbed19fca96b
Ashish Kapoor <swiftobjc@gmail.com>
2017-04-24 11:53:14 +0530

    added system time with utilization of darksky api

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -8,14 +8,22 @@ let weather = WeatherController()
 weather.addRoutes(drop:drop)
 
 drop.get { request in
-    let json = try drop.client.get("https://api.darksky.net/forecast/\(darkSkyKey)/37.8267,-122.4233", query:["exclude":"minutely,daily,alerts,flags"])
     
+    let darkSkyKey  = "7a100d1586e740565de176a0ed1c509e"
+    let darkSkyURL  = "https://api.darksky.net/forecast/\(darkSkyKey)/37.8267,-122.4233"
+    let json        = try drop.client.get(darkSkyURL, query:["exclude":"minutely, daily, alerts, flags"])
     let body        = try JSON(bytes: json.body.bytes!)
     let currently   = body["currently"]! as JSON
     let hourly      = body["hourly"]! as JSON
     
-    return try JSON(node: ["currently": currently,
-                           "hourly": hourly])
+    return try drop.view.make("welcome", Node(node:
+        [
+            "time"               : TimeKeeper().getSystemTime(),
+            "temp"               : currently["apparentTemperature"],
+             "current-summary"   : currently["summary"],
+             "hourly-summary"    : hourly["summary"],
+             "icon"              : currently["icon"]
+        ]))
 }
 
 drop.run()
